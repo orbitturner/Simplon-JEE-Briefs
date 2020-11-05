6ca2a6a0e5939f24ab6d7260ea33df3e0e6e7f53
orbitturner <orbitturner@gmail.com>
2020-11-05 12:46:05 +0000

    Creating Spring SenForage Project

---
+++ IntelliJ_WORKSPACE/SenForageSpringWeb/src/test/java/com/senforage/orbit/SenforagespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senforage.orbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenforagespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
