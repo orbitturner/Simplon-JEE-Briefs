611d08638c5f2228557d4fd8c802c94a23853bfc
orbitturner <orbitturner@gmail.com>
2020-11-13 12:06:02 +0000

    SECURITY INTEGRATION

--- IntelliJ_WORKSPACE/SenForageSpringWeb/src/main/java/com/senforage/orbit/security/SecurityConfig.java
+++ IntelliJ_WORKSPACE/SenForageSpringWeb/src/main/java/com/senforage/orbit/security/SecurityConfig.java
@@ -15,9 +15,28 @@ package com.senforage.orbit.security;
  *                              GITHUB : /orbitturner    -   Website: http://orbitturner.com/
  */
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Specify & Get The DataSource where The Users Are
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
